96674cd500e112270f0bf3be254707b878432f2e
yasin <yasin.elsharawi23@gmail.com>
2022-10-08 16:15:41 +0300

    Set chapters default value to 0

--- db/migrate/20221008104053_create_books.rb
+++ db/migrate/20221008104053_create_books.rb
@@ -2,7 +2,7 @@ class CreateBooks < ActiveRecord::Migration[7.0]
   def change
     create_table :books do |t|
       t.string :title
-      t.integer :chapters
+      t.integer :chapters, default: 0
       t.string :image_url
       t.text :description
       t.string :category
