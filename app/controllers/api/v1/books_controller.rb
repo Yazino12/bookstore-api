24e6ba263783dc877e0fa0c46ab6efd885b2c725
yasin <yasin.elsharawi23@gmail.com>
2022-10-11 16:16:14 +0300

    Fix linters

--- app/controllers/api/v1/books_controller.rb
+++ app/controllers/api/v1/books_controller.rb
@@ -1,10 +1,8 @@
 class Api::V1::BooksController < ApplicationController
-    def index
-        @books = Book.first
-        render json: { data: @books }, status: :ok
-    end
-  
-    def show; end
+  def index
+    @books = Book.first
+    render json: { data: @books }, status: :ok
+  end
 
+  def show; end
 end
-  
