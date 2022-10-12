cea9c4f3ec363520673c5888d2e821d726d1bc74
yasin <yasin.elsharawi23@gmail.com>
2022-10-12 19:32:32 +0300

    Remove new fromt comments routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :books, only: [:index, :show]
-      resources :comments, only: [:index, :show, :create, :new, :destroy]
+      resources :comments, only: [:index, :show, :create, :destroy]
     end
   end
 
