6a4ac28d8d9de75d1bd51ee9b21eb2b2bca9ae27
yasin <yasin.elsharawi23@gmail.com>
2022-10-13 20:24:35 +0300

    Add index show create update destroy actions for books

--- app/controllers/api/v1/books_controller.rb
+++ app/controllers/api/v1/books_controller.rb
@@ -1,8 +1,47 @@
 class Api::V1::BooksController < ApplicationController
   def index
-    @books = Book.first
+    @books = Book.all
     render json: { data: @books }, status: :ok
+    # render json: BookSerializer.new(@books), adapter: :json
   end
 
-  def show; end
+  def show
+    @book = Book.find_by_id(params[:id])
+    render json: { data: @book }, status: :ok
+  end
+
+  def create
+    @new_book = Book.new(book_params)
+    
+    if @new_book.save
+      render json: { data: @new_book }, status: :ok
+    else
+      render json: {error: @new_book.errors.messages }, status: 422
+    end
+  end
+
+  def update
+    @update_book = Book.find_by_id(params[:id])
+
+    if @update_book.update!(book_params)
+      render json: { data: @update_book }, status: :ok
+    else
+      render json: {error: @update_book.errors.messages }, status: 422
+    end
+  end
+    
+  def destroy
+    @delete_book = Book.find_by_id(params[:id])
+
+    if !@delete_book.destroy
+      render json: {error: @delete_book.errors.messages }, status: 422
+  end
+end
+
+  private
+
+  def book_params
+    params.require(:book).permit(:title, :chapters, :image_url, :description, :category, :author)
+  end
+
 end
