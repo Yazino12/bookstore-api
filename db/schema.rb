284cdb67af7dfb1bfe3e1ab320400e6bb8cf5966
yasin <yasin.elsharawi23@gmail.com>
2022-10-22 14:30:34 +0300

    Add user with default profile

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_10_10_152638) do
+ActiveRecord::Schema[7.0].define(version: 2022_10_22_112354) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -31,6 +31,13 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_10_152638) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["book_id"], name: "index_comments_on_book_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "profile", default: "https://cdn4.iconfinder.com/data/icons/success-filloutline/64/portfolio-business_and_finance-personal_profile-resume-interface-512.png"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   add_foreign_key "comments", "books"
