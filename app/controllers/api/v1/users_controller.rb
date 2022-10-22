330ada374d12993a0448f03c8e481db45098e628
yasin <yasin.elsharawi23@gmail.com>
2022-10-22 14:38:41 +0300

    Add users controller with index and show actions

---
+++ app/controllers/api/v1/users_controller.rb
@@ -0,0 +1,13 @@
+class Api::V1::UsersController < ApplicationController
+    def index
+      @users = User.all
+      render json: { data: @users }, status: :ok
+    end
+  
+    def show
+      @user = User.find_by_id(params[:id])
+      render json: { data: @user }, status: :ok
+    end
+  
+  end
+  
